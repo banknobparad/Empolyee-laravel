e092c42fab7687fc506b00502f685b2d5c2d7996
banknobparad <nobparadsaetun5@gmail.com>
2023-12-13 15:10:10 +0700

    first commit

---
+++ database/migrations/2023_12_13_073447_create_empolyees_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('empolyees', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unique();
+            $table->string('name');
+            $table->string('tel');
+            $table->string('card_num');
+
+
+            $table->string('depant_id');
+            $table->string('branch_id');
+
+            $table->datetime('start_time');
+            $table->text('address');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('empolyees');
+    }
+};
